0ece4065465fdd1e598b320d54f433ed4a063f89
Joel Chippindale <joel@joelchippindale.com>
2020-03-22 14:42:05 +0000

    Collect information about all the photos

--- build_migration_photos_list.py
+++ build_migration_photos_list.py
@@ -4,52 +4,46 @@ import pickle
 import os
 from pathlib import Path
 import redis
+import json
 
 r = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
 flickr_api.set_keys(api_key=os.environ['FLICKR_API_KEY'], api_secret=os.environ['FLICKR_API_SECRET'])
 
-photoset_counter = 0
 flickr_api.set_auth_handler((Path.cwd() / "auth" / "flickr_credentials.dat").resolve().as_posix())
 user = flickr_api.test.login()
 
-photo_set_walker = flickr_api.Walker(user.getPhotosets)
-for photo_set in photo_set_walker:
-    my_photos = []
+photo_counter = 0
 
-    print(f"photoset: {photo_set} {photoset_counter}")
+def photo_key(photo):
+    return f"photo-{photo['id']}"
 
-    if r.get(photo_set['id']) is None:
-        counter = 0
+photo_walker = flickr_api.Walker(user.getPhotos)
+for photo in photo_walker:
 
-        photo_walker = flickr_api.Walker(photo_set.getPhotos)
-        for photo in photo_walker:
-            counter += 1
-            print(f"{counter} {photo}")
-            try:
-                photo_url = photo.getPhotoFile("Original")
-            except flickr_api.flickrerrors.FlickrServerError as e:
-                print(f"Couldn't get original size URL {e}. Skipping")
-                continue
-            except flickr_api.flickrerrors.FlickrError as e:
-                print(f"Couldn't get original size URL {e}. Skipping")
-                continue
+    print(f"photo: {photo_counter} - {photo.getInfo()}")
 
-            my_photos.append({
-                "album": photo_set['title'],
-                "photoId": photo['id'],
-                "photoTitle": photo['title'],
-                "photoUrl": photo_url,
-                "processed": False,
-                "photoTags": ','.join([t['text'] for t in photo.getTags()]),
-            })
+    if r.get(photo_key(photo)) is None:
+        try:
+            photo_url = photo.getPhotoFile("Original")
+        except flickr_api.flickrerrors.FlickrServerError as e:
+            print(f"Couldn't get original size URL {e}. Skipping")
+            continue
+        except flickr_api.flickrerrors.FlickrError as e:
+            print(f"Couldn't get original size URL {e}. Skipping")
+            continue
 
-        print(f"writing photoset {photoset_counter}")
-        with open((Path.cwd() / f"photosets-queue/photoset-{photo_set['id']}-{counter}.pickle").resolve(), "wb") as photo_tasks_file:
-            pickle.dump(my_photos, photo_tasks_file)
+        info = {
+            "id": photo['id'],
+            "title": photo['title'],
+            "photoUrl": photo_url,
+            "processed": False,
+            "description": photo.getInfo()['description'],
+            "tags": photo.getTags(),
+            "taken": photo.getInfo()['taken']
+        }
 
-        r.set(photo_set['id'], len(my_photos))
+        print(f"info {info}")
 
-    photoset_counter += 1
+        r.set(photo_key(photo), json.dumps(info))
 
-
-
+        photo_counter += 1
